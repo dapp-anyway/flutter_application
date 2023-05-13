8612f3bc16ac8166982a245d6de159e0a560d5e5
Rashid Galiullin <dapp.anyway@gmail.com>
2023-05-13 07:23:47 +0300

    Update Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:chuck_norris_application/views/categories_page.dart';
-import 'package:chuck_norris_application/views/home_page.dart';
-import 'package:chuck_norris_application/views/joke_page.dart';
 import 'package:flutter/material.dart';
 import 'models/wrapper.dart';
-
 
 // flutter build apk --release --no-sound-null-safety  --build-name=1.0 --build-number=1
 
@@ -20,6 +16,4 @@ class MyApp extends StatelessWidget {
       home: Wrapper(),
     );
   }
-
-
 }
